f7ea19da01c844d46da09750c5dc1b266cd07d3e
SteveKommrusch <steve.kommrusch@gmail.com>
2020-05-23 17:05:55 -0600

    Adding data test files.

--- src/srcvaltest.sh
+++ src/srcvaltest.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-if [ ! -f ../../data/$1_multipass.txt ]; then
+if [ ! -f $1 ]; then
     echo "Usage: srcvaltest.sh name"
-    echo "       Creates name_trainval.txt and name_test.txt files if needed"
-    echo "       Createst src-* and tgt-* files for OpenNMT use"
+    echo "       Creates src-*, val-*, and tgt-* files for OpenNMT use."
     exit 0
 fi
-f=../../data/$1
+f=$1
 
-if [ ! -f ${f}_trainval.txt ]; then
-    perl -ne '/Y (.*) Z/ && ($this=$1); if ((!$last || $last ne $this)) { $n++; if ($n % 50 != 10) {$p=1} else {$p=0}}; $p && print; $last=$this' ${f}_multipass.txt | shuf > ${f}_trainval.txt
-    perl -ne '/Y (.*) Z/ && ($this=$1); if ((!$last || $last ne $this)) { $n++; if ($n % 50 == 10) {print}}; $last=$this' ${f}_multipass.txt | head -n 10000 > ${f}_test.txt
-fi
+perl -ne '/Y (.*) Z/; $this=$1; if ((!$last || $last ne $this)) { $n++; if ($n % 50 != 10) {$p=1} else {$p=0}}; $p && print; $last=$this' $f | shuf > all_trainval.txt
+perl -ne '/Y (.*) Z/; $this=$1; if ((!$last || $last ne $this)) { $n++; if ($n % 50 == 10) {print}}; $last=$this' $f | head -n 10000 > all_test.txt
 
-perl -ne '/^(.*) X / && print $1."\n"' ${f}_trainval.txt > src.txt
-head -n 1000000 src.txt > src-train.txt
-head -n 1010000 src.txt | tail -n 10000 > src-val.txt
-perl -ne '/^(.*) X / && print $1."\n"' ${f}_test.txt | head -n 10000 > src-test.txt
-perl -ne '/Z (.*)\s*$/ && print $1."\n"' ${f}_trainval.txt > tgt.txt
-head -n 1000000 tgt.txt > tgt-train.txt
-head -n 1010000 tgt.txt | tail -n 10000 > tgt-val.txt
-perl -ne '/Z (.*)\s*$/ && print $1."\n"' ${f}_test.txt | head -n 10000 > tgt-test.txt
+perl -ne '/^(.*) X / && print $1."\n"' all_trainval.txt | head -n 1000000 > src-train.txt
+perl -ne '/^(.*) X / && print $1."\n"' all_trainval.txt | tail -n 10000 > src-val.txt
+perl -ne '/^(.*) X / && print $1."\n"' all_test.txt > src-test.txt
+perl -ne '/Z (.*)\s*$/ && print $1."\n"' all_trainval.txt | head -n 1000000 > tgt-train.txt
+perl -ne '/Z (.*)\s*$/ && print $1."\n"' all_trainval.txt | tail -n 10000 > tgt-val.txt
+perl -ne '/Z (.*)\s*$/ && print $1."\n"' all_test.txt > tgt-test.txt
+
+ln -s ${f/all_/raw_} raw_link
+perl -e 'open($all,"<","all_test.txt"); while (<$all>) { /X (.* Y .*) Z (.*)$/ || die; $full{$1}=1; }; open($raw,"<","raw_link"); while (<$raw>) { /X (.* Y .*) Z (.*)$/ || die; if ($full{$1}) { print } }' > all_test_fullaxioms.txt
+
+../../src/possibleAxioms.pl all_test.txt > all_test_possible.txt
+perl -ne '/Y (.*) (.*) Z/ || die; $c[$1][$2] += 1; if ($. == 10000) { for ($i=0; $i< 56; $i++) { printf "%3d ",$i; for ($j=1; $j < 36; $j++) { if (!$i) { printf "%4d",$j; } else { printf "%4d",$c[$i][$j]+0 } } print "\n"; } }' all_test_possible.txt > table.txt
 
