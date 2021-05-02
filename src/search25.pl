361457b3c1efeaf706c8f9378497a5c724636135
SteveKommrusch <steve.kommrusch@gmail.com>
2021-05-02 08:42:49 -0600

    Improved tuning find script

---
+++ src/search25.pl
@@ -0,0 +1,158 @@
+#!/usr/bin/perl
+#
+use strict;
+use warnings;
+
+require "../src/genProgUsingAxioms.pl";
+
+if ( ! -f $ARGV[2] || ! -d $ARGV[4] ) {
+  print "Usage: search25.pl beam maxtok src model dir \n";
+  print "    Open source file and search 25 steps to see if model can prove\n";
+  print "    programs equal using beam width and up to maxtok for both programs.\n";
+  print "  Example: search.pl 5 all_multi_test.txt final-model_step_100000.pt tr_x\n";
+  exit(1);
+}
+my $model=$ARGV[3];
+if ( ! -f $model ) {
+  print "Error: $model must be a model file\n";
+  exit(1);
+}
+
+my $beam=$ARGV[0];
+my $maxTokens=$ARGV[1];
+open(my $fh_all,"<",$ARGV[2]) || die "open fh_all failed: $!";
+my $dir=$ARGV[4];
+
+my $pos=0;
+my $neg=0;
+my $exactpos=0;
+my $tpos=0;
+my $tneg=0;
+my @progAs;
+my @progBs;
+my @tgt;
+my @axpath;
+my @axioms;
+my %searching;
+my @nsrc;
+my $progA;
+my $progB;
+my $legal=0;
+my $illegal=0;
+my $badsyntax=0;
+
+my $lnum=0;
+while (<$fh_all>) {
+  $lnum++;
+  /X (.*) Y (.*) Z (.*)$/ || die "Error: incorrect syntax on input file: $_\n";
+  $progA=$1;
+  $progB=$2;
+  $tgt[$lnum]=$3;
+  $progAs[$lnum][1][1]=$progA; # Dims are sample,axiom,beam
+  $nsrc[$lnum]=1;              # Current number of active beam
+  $progBs[$lnum]=$progB;
+  $axioms[$lnum][1][1]="";
+  $axpath[$lnum]="";           # No proven path yet
+  $searching{$lnum.$progA}=1;  # Allows quick equiv check
+}
+close($fh_all) || die "close fh_all failed: $!";
+
+for (my $axsteps=1; $axsteps <= 25; $axsteps++) {
+  open(my $fh_raw,">","$dir/search_raw$axsteps.txt") || die "open fh_raw failed: $!";
+  for (my $i=1; $i <= $lnum; $i++) {
+    for (my $j=1; $j <= $nsrc[$i]; $j++) {
+      $progA = $progAs[$i][$axsteps][$j];
+      print $fh_raw "X $progA Y $progBs[$i] Z Emptyprediction\n";
+    }
+  }
+  close($fh_raw) || die "close fh_raw failed: $!";
+  # system "../../src/pre2graph.pl < search_raw$axsteps.txt > search_all$axsteps.txt\n";
+  # system "perl -ne '/^(.*) X / && print \$1.\"\\n\"' search_all$axsteps.txt > search_src-test$axsteps.txt";
+  system "perl -ne '/X (.*) Z / && print \$1.\"\\n\"' $dir/search_raw$axsteps.txt > $dir/search_src-test$axsteps.txt";
+  system "onmt_translate -model $model -src $dir/search_src-test$axsteps.txt -output $dir/search_pred$axsteps.txt -gpu 0 -replace_unk -beam_size 5 -n_best 5 -batch_size 4 -verbose > $dir/search_translate$axsteps.out 2>&1";
+  
+  open(my $fh_pred,"<","$dir/search_pred$axsteps.txt") || die "open fh_pred failed: $!";
+  for (my $i=1; $i <= $lnum; $i++) {
+    my $new_nsrc=0;
+    my @preds;
+    my $found=0;
+    $progB = $progBs[$i];
+    my $numtokB = (scalar split /[;() ]+/,$progB);
+    for (my $j=1; $j <= $nsrc[$i]; $j++) {
+      for (my $k=1; $k <= 5; $k++) {
+        my $ln = <$fh_pred> || die "Unexpected end of fh_pred";
+        $ln =~s/\s*$//;
+        $preds[$j][$k]=$ln;
+      }
+    }
+    # Process predictions prioritizing 'best' predictions for each sample
+    for (my $k=1; $k <= 5; $k++) {
+      for (my $j=1; $j <= $nsrc[$i] && $new_nsrc < ($beam > 2 ? 5*$beam : $beam) && !$found; $j++) {
+        $progA=$progAs[$i][$axsteps][$j];
+        my $ln = $preds[$j][$k];
+        my $stm="";
+        my $axiom="";
+        my $args="";
+        if ($ln =~ /^(stm\d+) ([A-Z][a-z]+)(.*)$/ ) {
+          $stm=$1;
+          $axiom=$2;
+          $args=$3;
+        }
+        # Syntax check before legality attempt
+        if (($axiom =~ /^(Swapprev|Deletestm)$/ && $args eq "") ||
+          ($axiom =~ /^(Inline|Usevar)$/ && $args=~/^ [mvs]\d+\s*$/) ||
+          ($axiom =~ /^(Newtmp)$/ && $args=~/^ N[lr]* [mvs]\d+\s*$/) ||
+          ($axiom =~ /^(Cancel|Noop|Double|Multzero|Commute|Distribleft|Distribright|Factorleft|Factorright|Assocleft|Assocright|Flipleft|Flipright|Transpose)$/ && $args=~/^ N[lr]*\s*$/)) {
+          my $predB = GenProgUsingAxioms($progA,"",$ln." ");
+          $predB=~s/\s*$//;
+          if ($predB ne $progA && !($predB=~/FAILTOMATCH/)) {
+            $legal++;
+            my $progTmp=$predB;
+            $progTmp=~s/[^()]//g;
+            while ($progTmp =~s/\)\(//g) {};
+            # Only add new programs which fit in maxToken (network size)
+            # And fewer than 21 statements and depth less than 7
+            if (!$searching{$i.$predB} && !($predB=~/TOODEEP/) && ($numtokB + (scalar split /[;() ]+/,$predB) < $maxTokens) && int(grep { /=/ } split / /,$predB) < 21 && length($progTmp)/2 < 7) {
+              $new_nsrc++;
+              $axioms[$i][$axsteps+1][$new_nsrc] = $axioms[$i][$axsteps][$j]." $ln";
+              if ($new_nsrc <= $beam) {
+                $searching{$i.$predB}=1;
+                $progAs[$i][$axsteps+1][$new_nsrc] = $predB;
+              }
+              # Check up to twice as many predictions as beam width
+              # This guarantees that at all of the 2nd-best guesses of
+              # the samples get checked.
+              if ($predB eq $progB) {
+                # Found path!
+                $axpath[$i] = $axioms[$i][$axsteps+1][$new_nsrc];
+                $found=1;
+                $new_nsrc = 0;   # No need to keep searching
+              }
+            }
+          } else {
+            $illegal++;
+          }
+        } else {
+          $badsyntax++;
+        }
+      }
+    }
+    $nsrc[$i] = $new_nsrc > $beam ? $beam: $new_nsrc; 
+  }
+  <$fh_pred> && die "Too many lines in fh_pred";
+  close($fh_pred) || die "close fh_pred failed: $!";
+}
+
+for (my $i=1; $i <= $lnum; $i++) {
+  if ($axpath[$i]) {
+    print "FOUND: $progAs[$i][1][1] to $progBs[$i] with $axpath[$i] Target path: $tgt[$i]\n";
+  } else {
+    for (my $j=1; $j <= 26; $j++) {
+        if (! exists $axioms[$i][$j+1][1]) {
+            print "FAIL: $progAs[$i][1][1] to $progBs[$i] bestguess after $j steps: $axioms[$i][$j][1] Target path: $tgt[$i]\n";
+            last;
+        }
+    }
+  }
+}
+print "Legal axiom proposals: $legal; Bad syntax: $badsyntax; Illegal axiom proposals: $illegal\n";
--- src/srcvaltest_find.sh
+++ src/srcvaltest_find.sh
@@ -9,11 +9,11 @@ fi
 rm -f */search_[sptr][ra]*.??t
 rm -f raw_tune.txt raw_xtra.txt
 for i in b1 b2 b3 b4 b5 b6 b7 b8 b9 b10; do
-  cat $i/tune25_2.txt $i/tune25_20.txt | perl -ne '/^Legal axiom prop/ && ($l=1); if ($l) {if (/FOUND: (.*) with\s+(.*) Target path:\s+(.*)$/) { $t=$3; $f=$2; $p=$1; $n=scalar split /stm/,$f; s/FOUND:/X/; s/ to / Y /; s/ with\s+/ Z /; s/Target path:.*$//; if (! exists $fndsml{$p}) {print} elsif ($n < (scalar split /stm/,$t)) { $ax[$n]++; if (/stm20/ || /stm19/ || / N[lr][lr][lr]/ || / Newtmp/ || / Factor/) {print} else {$ax[$n] <= ($n*5+10) && print }}}} else {/FOUND: (.*) with/ && ($fndsml{$1}=1);}' >> raw_tune.txt
+  cat $i/tune25_2.txt $i/tune25_20.txt | perl -ne '/^Legal axiom prop/ && ($l=1); if (/FOUND: (.*) with\s+(.*) Target path:\s+(.*)$/) { $t=$3; $f=$2; $p=$1; $n=scalar split /stm/,$f; if ($l) {s/FOUND:/X/; s/ to / Y /; s/ with\s+/ Z /; s/Target path:.*$//; if (! exists $fndsml{$p} && $n <= (2+ scalar split /stm/,$t)) {print} elsif ($n < (scalar split /stm/,$t)) { $ax[$n]++; if (/stm20/ || /stm19/ || / N[lr][lr][lr]/ || / Newtmp/ || / Factor/) {print} else {$ax[$n] <= ($n*5+10) && print }}} else {$n <= (2+ scalar split /stm/,$t) && ($fndsml{$p}=1);}}' >> raw_tune.txt
 
   perl -ne 'if (/FAIL.*Target.* N[lr][lr][lr][lr]/ && ! /Target.*stm.*stm.*stm.*stm.*stm.*stm.*stm.*stm.*stm.*stm.*stm.*stm.*stm.*stm.*stm.*stm.*stm.*stm.*stm.*stm.*stm/) { s/FAIL:/X/; s/ to / Y /; s/ bestguess.* Target path:\s+/ Z /; print }' $i/tune25_20.txt >> raw_xtra.txt
 done
-cat reversed/tune10_2.txt reversed/tune10_20.txt | perl -ne '/^Legal axiom prop/ && ($l=1); if ($l) {if (/FOUND: (.*) with\s+(.*) Target path:\s+(.*)$/) { $t=$3; $f=$2; $p=$1; $n=scalar split /stm/,$f; s/FOUND:/X/; s/ to / Y /; s/ with\s+/ Z /; s/Target path:.*$//; if (! exists $fndsml{$p}) {print} else { $ax[$n]++; if (/stm20/ || /stm19/ || / N[lr][lr][lr]/ || / Newtmp/ || / Factor/) {print} else {$ax[$n] <= ($n*5+10) && print }}}} else {/FOUND: (.*) with/ && ($fndsml{$1}=1);}' >> raw_tune.txt
+cat reversed/tune10_2.txt reversed/tune10_20.txt | perl -ne '/^Legal axiom prop/ && ($l=1); if ($l) {if (/FOUND: (.*) with\s+(.*) Target path:\s+(.*)$/) { $t=$3; $f=$2; $p=$1; $n=scalar split /stm/,$f; s/FOUND:/X/; s/ to / Y /; s/ with\s+/ Z /; s/Target path:.*$//; if (! exists $fndsml{$p}) {print} else { $ax[$n]++; if (/stm20/ || /stm19/ || / N[lr][lr][lr]/ || / Newtmp/ || / Factor/) {print} else {$ax[$n] <= ($n*5) && print }}}} else {/FOUND: (.*) with/ && ($fndsml{$1}=1);}' >> raw_tune.txt
 
 ../../../src/pre1axiom.pl 225 raw_tune.txt > pre1axiom_tune.out
 ../../../src/pre1axiom.pl 225 raw_xtra.txt | grep " N[lr][lr][lr][lr]" >> pre1axiom_tune.out
